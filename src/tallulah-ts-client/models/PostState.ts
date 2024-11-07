98284bf472228c38fb7760e7b97a5728ce63973e
Muthu Subramanian <muthu@arrayinsights.com>
2024-11-07 20:59:11 +0530

    fix: state update

--- src/tallulah-ts-client/models/PostState.ts
+++ src/tallulah-ts-client/models/PostState.ts
@@ -3,8 +3,9 @@
 /* eslint-disable */
 
 export enum PostState {
-  REQUESTED = 'REQUESTED',
-  IN_PROGRESS = 'IN PROGRESS',
-  APPROVED = 'APPROVED',
-  DENIED = 'DENIED'
+    IN_QUEUE = 'IN QUEUE',
+    REQUESTED = 'REQUESTED',
+    IN_PROGRESS = 'IN PROGRESS',
+    APPROVED = 'APPROVED',
+    DENIED = 'DENIED',
 }

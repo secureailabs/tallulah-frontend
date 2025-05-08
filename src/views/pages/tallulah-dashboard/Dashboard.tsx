2a5113220d219ab9b41b753b3e62c211e63289e1
Muthu Subramanian <muthu@arrayinsights.com>
2025-05-08 05:32:01 +0000

    If not logged in go to login page

--- src/views/pages/tallulah-dashboard/Dashboard.tsx
+++ src/views/pages/tallulah-dashboard/Dashboard.tsx
@@ -1,13 +1,34 @@
-'use client';
+'use client'
 
-import { Box } from '@mui/material';
-import styles from './Dashboard.module.css';
-import PatientStoryDashboard from './components/PatientStoryDashboard';
-import StaticDashboard from './components/StaticDashboard/StaticDashboard';
+import { Box } from '@mui/material'
+import styles from './Dashboard.module.css'
+import PatientStoryDashboard from './components/PatientStoryDashboard'
+import StaticDashboard from './components/StaticDashboard/StaticDashboard'
+import { useEffect } from 'react'
+import { AuthenticationService } from '@/tallulah-ts-client'
 
 export interface IDashboard {}
 
 const Dashboard: React.FC<IDashboard> = ({}) => {
+  useEffect(() => {
+    // If not authenticated, redirect to login page
+    const accessToken = localStorage.getItem('access_token')
+    if (!accessToken) {
+      window.location.href = '/login'
+    }
+    // call /me api to get user info
+    AuthenticationService.getCurrentUserInfo()
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => {
+        console.log(err)
+        // If token expired, redirect to login page
+        if (err.response.status === 401) {
+          window.location.href = '/login'
+        }
+      })
+  }, [])
   return (
     <Box>
       {/* Overview of all the dashboard */}
@@ -17,15 +38,15 @@ const Dashboard: React.FC<IDashboard> = ({}) => {
 
       <Box
         sx={{
-          marginBottom:'50px'
+          marginBottom: '50px'
         }}
       >
-      <StaticDashboard />
+        <StaticDashboard />
       </Box>
       {/* Dynamic Dashboard */}
       <PatientStoryDashboard />
     </Box>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
